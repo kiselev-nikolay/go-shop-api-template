c22e6086648cd766348b18001163d3947b1007bb
Nikolay Kiselev <55307887+kiselev-nikolay@users.noreply.github.com>
2021-05-14 12:32:48 +0300

    Add catalogue controller

--- catalogue/controllers/controllers.go
+++ catalogue/controllers/controllers.go
@@ -1,1 +1,80 @@
 package controllers
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kiselev-nikolay/go-shop-api-template/catalogue/models"
+	"gorm.io/gorm"
+)
+
+func New(DB *gorm.DB) *ctrl {
+	return &ctrl{DB: DB}
+}
+
+type ctrl struct {
+	DB *gorm.DB
+}
+
+func (c *ctrl) Connect(group *gin.RouterGroup) {
+	group.POST("/product", c.Product)
+}
+
+type CreatorReq struct {
+	ID uint `json:"id"`
+}
+
+type CategoryReq struct {
+	Name string `json:"name"`
+}
+
+type ProductReq struct {
+	Code       string        `json:"code"`
+	Price      uint          `json:"price"`
+	Name       string        `json:"name"`
+	Creator    CreatorReq    `json:"creator"`
+	Categories []CategoryReq `json:"categories"`
+}
+
+type Detail struct {
+	Value string `json:"detail"`
+}
+
+func (c *ctrl) Product(g *gin.Context) {
+	req := &ProductReq{}
+	b, _ := g.Request.GetBody()
+	x, _ := ioutil.ReadAll(b)
+	fmt.Println(string(x))
+	bindErr := g.Bind(req)
+	if bindErr != nil {
+		g.JSON(400, Detail{"wrong body"})
+		return
+	}
+	fmt.Println(req.Creator.ID)
+	creatorSearch := c.DB.First(&models.Creator{}, req.Creator.ID)
+	if errors.Is(creatorSearch.Error, gorm.ErrRecordNotFound) {
+		g.JSON(400, Detail{"creator invalid"})
+		return
+	}
+	cats := make([]models.Category, 0)
+	for _, cat := range req.Categories {
+		cats = append(cats, models.Category{
+			Name: cat.Name,
+		})
+	}
+	p := &models.Product{
+		Code:       req.Code,
+		Price:      req.Price,
+		Name:       req.Name,
+		CreatorID:  uint(req.Creator.ID),
+		Categories: cats,
+	}
+	productCreate := c.DB.Create(p)
+	if errors.Is(productCreate.Error, gorm.ErrRecordNotFound) {
+		g.JSON(400, Detail{"not created"})
+		return
+	}
+	g.JSON(200, Detail{"created"})
+}
--- catalogue/controllers/controllers_test.go
+++ catalogue/controllers/controllers_test.go
@@ -1,1 +1,104 @@
 package controllers_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kiselev-nikolay/go-shop-api-template/catalogue/controllers"
+	"github.com/kiselev-nikolay/go-shop-api-template/catalogue/models"
+	"github.com/kiselev-nikolay/go-shop-api-template/tools/testtools"
+	"gotest.tools/assert"
+)
+
+func TestViews(t *testing.T) {
+	db, stop := testtools.MustCreateDB()
+	defer stop()
+
+	err := db.AutoMigrate(&models.Creator{})
+	assert.NilError(t, err)
+	err = db.AutoMigrate(&models.Category{})
+	assert.NilError(t, err)
+	err = db.AutoMigrate(&models.Product{})
+	assert.NilError(t, err)
+
+	creator := &models.Creator{
+		Name: "test creator",
+	}
+	db.Create(creator)
+
+	product := models.Product{
+		Code:      "abc",
+		Price:     100,
+		Name:      "test",
+		CreatorID: creator.ID,
+		Categories: []models.Category{
+			{Name: "test cat 1"},
+			{Name: "test cat 2"},
+			{Name: "test cat 3"},
+		},
+	}
+	db.Create(&product)
+	db.Save(&product)
+
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+	c := controllers.New(db)
+	router.POST("/product", c.Product)
+
+	t.Run("Create with invalid creator", func(t *testing.T) {
+		data, _ := json.Marshal(controllers.ProductReq{
+			Code:  "scp096",
+			Price: 200,
+			Name:  "Strange Human Size Box",
+			Creator: controllers.CreatorReq{
+				ID: 29,
+			},
+			Categories: []controllers.CategoryReq{},
+		})
+		req, _ := http.NewRequest("POST", "/product", bytes.NewBuffer(data))
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req)
+		res := &controllers.Detail{}
+		bodyData, err := ioutil.ReadAll(w.Body)
+		assert.NilError(t, err)
+		err = json.Unmarshal(bodyData, res)
+		assert.NilError(t, err)
+		assert.Equal(t, 400, w.Result().StatusCode)
+		assert.DeepEqual(t, res, &controllers.Detail{"creator invalid"})
+	})
+
+	t.Run("Create", func(t *testing.T) {
+		creator := &models.Creator{
+			Name: "unknown",
+		}
+		db.Create(creator)
+
+		data, _ := json.Marshal(controllers.ProductReq{
+			Code:  "scp096",
+			Price: 200,
+			Name:  "Strange Human Size Box",
+			Creator: controllers.CreatorReq{
+				ID: creator.ID,
+			},
+			Categories: []controllers.CategoryReq{},
+		})
+		req, _ := http.NewRequest("POST", "/product", bytes.NewBuffer(data))
+		req.Header.Add("Content-Type", "application/json")
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req)
+		res := &controllers.Detail{}
+		bodyData, err := ioutil.ReadAll(w.Body)
+		assert.NilError(t, err)
+		fmt.Println(string(bodyData))
+		err = json.Unmarshal(bodyData, res)
+		assert.NilError(t, err)
+		assert.Equal(t, 200, w.Result().StatusCode)
+		assert.DeepEqual(t, res, &controllers.Detail{"created"})
+	})
+}
